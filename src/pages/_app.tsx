d2aec686df2a27516df75946372e5eb0e9f8d695
Arjun Tanwar <arjunthakur900@gmail.com>
2023-04-09 20:42:07 +0530

    start fresh

--- src/pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-import '@/styles/globals.css'
-import { trpcClient } from '@/utils/trpc'
-import type { AppProps } from 'next/app'
-
-function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
-
-export default trpcClient.withTRPC(App)
